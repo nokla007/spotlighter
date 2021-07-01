f5e1209ae26c8f01c6cd5e3e313a3fdb22d23735
Ashikur Rahman <45009990+ashik061@users.noreply.github.com>
2021-07-02 00:45:09 +0600

    task page implemented.

--- lib/screens/tasks_page.dart
+++ lib/screens/tasks_page.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
 
 class TasksPage extends StatelessWidget {
-  const TasksPage({Key? key}) : super(key: key);
+  TasksPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('this is tasks')),
+    return ListView(children: [
+      TaskTile(title: 'Task 1'),
+      TaskTile(title: 'Task 1'),
+      TaskTile(title: 'Task 1'),
+      TaskTile(title: 'Task 1'),
+      TaskTile(title: 'Task 1'),
+      TaskTile(title: 'Task 1'),
+      TaskTile(title: 'Task 1'),
+      TaskTile(title: 'Task 1'),
+    ]);
+  }
+}
+
+class TaskTile extends StatefulWidget {
+  String title = '';
+  bool value = false;
+
+  TaskTile({Key? key, required this.title}) : super(key: key);
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Checkbox(
+          value: widget.value,
+          onChanged: (val) {
+            if (val != null)
+              setState(() {
+                widget.value = val;
+              });
+          },
+        ),
+        title: Text(
+          widget.title,
+          style: TextStyle(
+            fontSize: 18,
+            decoration: (widget.value) ? TextDecoration.lineThrough : null,
+          ),
+        ),
+        onTap: () {
+          print('task tapped');
+        },
+      ),
     );
   }
 }
